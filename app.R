1efe27782ea1e71f1e7ac7962ff4e8a420674fa3
Francisco J. Romero-Campero <fran@us.es>
2018-11-30 09:41:53 +0100

    Add edges to the network representation

--- app.R
+++ app.R
@@ -14,7 +14,6 @@ colnames(network.data)[46] <- "ATHB-40"
 colnames(network.data)[34] <- "ATHB-6"
 colnames(network.data)[33] <- "ATHB-21"
 
-
 ## Transforming coordinates for a better visualization
 x.coord <- as.numeric(network.data$y.pos)
 y.coord <- as.numeric(network.data$x.pos)
@@ -68,6 +67,9 @@ ui <- fluidPage(
                            label = "Select Transcription Factors:",
                            choices = sort(tfs.data$name,decreasing=F),
                            inline = TRUE,width = "100%"),
+        
+        checkboxInput(inputId = "edges",label = "Visualize edges",value = FALSE),
+        
         actionButton(inputId = "button_tfs",label = "Select Genes"),
         
         selectizeInput(inputId = "topological_parameter",
@@ -77,6 +79,7 @@ ui <- fluidPage(
                                    "Closeness",
                                    "Betweeness",
                                    "Eccentricity")),
+        
         conditionalPanel(condition = "input.topological_parameter == 'In degree'",
                          sliderInput(inputId = "indegree_range",
                                      label="Choose an in degree range:",
@@ -85,7 +88,6 @@ ui <- fluidPage(
                                      value=c(min(network.data$indegree),max(network.data$indegree))),
                          actionButton(inputId = "button_indegree",label = "Select Genes")
                          ),
-        
 
         conditionalPanel(condition = "input.topological_parameter == 'Transitivity'",
                          sliderInput(inputId = "transitivity_range",
@@ -96,6 +98,37 @@ ui <- fluidPage(
                          actionButton(inputId = "button_transitivity",label = "Select Genes")
         ),
 
+        conditionalPanel(condition = "input.topological_parameter == 'Closeness'",
+                         sliderInput(inputId = "closeness_range",
+                                     label="Choose a closeness range:",
+                                     min=min(network.data$closeness),
+                                     max=max(network.data$closeness),
+                                     value=c(min(network.data$closeness),max(network.data$closeness))),
+                         actionButton(inputId = "button_closeness",label = "Select Genes")
+        ),
+        
+        conditionalPanel(condition = "input.topological_parameter == 'Betweeness'",
+                         sliderInput(inputId = "betweeness_range",
+                                     label="Choose a betweeness range:",
+                                     min=min(network.data$betweenness),
+                                     max=max(network.data$betweenness),
+                                     value=c(min(network.data$betweeness),max(network.data$betweeness))),
+                         actionButton(inputId = "button_betweeness",label = "Select Genes")
+        ),
+        
+
+        conditionalPanel(condition = "input.topological_parameter == 'Eccentricity'",
+                         sliderInput(inputId = "eccentricity_range",
+                                     label="Choose an eccentricity range:",
+                                     min=min(network.data$eccentricity),
+                                     max=max(network.data$eccentricity),
+                                     value=c(min(network.data$eccentricity),max(network.data$eccentricity))),
+                         actionButton(inputId = "button_eccentricity",
+                                      label = "Select Genes")
+        ),
+        
+                
+                
 #         sliderInput(inputId = "degree_range", label = h3("Degree Range"), min = 0, 
 #                     max = 11, value = c(2, 4)),
 #         actionButton(inputId = "button_degree",label = "Select Genes"),
@@ -146,8 +179,6 @@ server <- function(input, output) {
   },height = 700)
   
 
-  
-  
   ## Visualization of selected genes according to their degree
   observeEvent(input$button_tfs, {
     print("aquí llego 4")
@@ -170,19 +201,60 @@ server <- function(input, output) {
     selected.nodes.colors <- selected.genes.df$color
     
     selected.tfs.df <- subset(network.data, names %in% tf.ids[input$selected.tfs])
-
-    output$networkPlot <- renderPlot({
-      ggplot(network.data, aes(x.pos,y.pos)) + 
-        theme(panel.background = element_blank(), 
-              panel.grid.major = element_blank(), 
+    
+    if(input$edges)
+    {
+      network.representation <- ggplot(network.data, aes(x.pos,y.pos)) +
+        theme(panel.background = element_blank(),
+              panel.grid.major = element_blank(),
               panel.grid.minor = element_blank(),
               axis.title = element_blank(),
               axis.text = element_blank(),
-              axis.ticks.y = element_blank()) + 
-        geom_point(color=network.data$color,size=1) +
-        geom_point(data = selected.tfs.df, size=8, fill=selected.tfs.df$color,colour="black",pch=21) +
-        geom_point(data = selected.genes.df,aes(x.pos,y.pos), size=4, fill=selected.nodes.colors,colour="black",pch=21) 
-    },height = 700)
+              axis.ticks.y = element_blank())
+      
+      network.representation <- ggplot(network.data, aes(x.pos,y.pos)) + 
+          theme(panel.background = element_blank(), 
+                panel.grid.major = element_blank(), 
+                panel.grid.minor = element_blank(),
+                axis.title = element_blank(),
+                axis.text = element_blank(),
+                axis.ticks.y = element_blank()) + 
+          geom_point(color=network.data$color,size=1) +
+          geom_point(data = selected.tfs.df, size=8, fill=selected.tfs.df$color,colour="black",pch=21) +
+          geom_point(data = selected.genes.df,aes(x.pos,y.pos), size=4, fill=selected.nodes.colors,colour="black",pch=21) 
+      
+      for(i in 1:length(input$selected.tfs))
+      {
+        tf.xpos <- subset(network.data, names == tf.ids[input_selected.tfs[i]])[["x.pos"]]
+        tf.ypos <- subset(network.data, names == tf.ids[input_selected.tfs[i]])[["y.pos"]]
+        network.representation <- network.representation +
+            annotate("segment",
+                     x=rep(tf.xpos,nrow(selected.genes.df)),
+                     y=rep(tf.ypos,nrow(selected.genes.df)),
+                     xend=selected.genes.df$x.pos,
+                     yend=selected.genes.df$y.pos, 
+                     color="grey", arrow=arrow(type="closed",length=unit(0.1, "cm")))
+      }
+      
+      output$networkPlot <- renderPlot({
+        network.representation
+      },height = 700)
+    } else
+    {
+      output$networkPlot <- renderPlot({
+        ggplot(network.data, aes(x.pos,y.pos)) + 
+          theme(panel.background = element_blank(), 
+                panel.grid.major = element_blank(), 
+                panel.grid.minor = element_blank(),
+                axis.title = element_blank(),
+                axis.text = element_blank(),
+                axis.ticks.y = element_blank()) + 
+          geom_point(color=network.data$color,size=1) +
+          geom_point(data = selected.tfs.df, size=8, fill=selected.tfs.df$color,colour="black",pch=21) +
+          geom_point(data = selected.genes.df,aes(x.pos,y.pos), size=4, fill=selected.nodes.colors,colour="black",pch=21) 
+      },height = 700)
+    }
+
     
     
     selected.genes.df$names <- gene.links[selected.genes.df$names]
@@ -255,8 +327,91 @@ server <- function(input, output) {
   })
   
   
-  
+  ## Visualization of selected genes according to their closeness
+  observeEvent(input$button_closeness, {
+    print("aquí llego 7")
     
+    close.values <- input$closeness_range
+    selected.genes.df <- subset(network.data, closeness >= close.values[1] & closeness <= close.values[2])
+    selected.nodes.colors <- selected.genes.df$color
+    
+    output$networkPlot <- renderPlot({
+      ggplot(network.data, aes(x.pos,y.pos)) +
+        theme(panel.background = element_blank(),
+              panel.grid.major = element_blank(),
+              panel.grid.minor = element_blank(),
+              axis.title = element_blank(),
+              axis.text = element_blank(),
+              axis.ticks.y = element_blank()) +
+        geom_point(color=network.data$color,size=1) +
+        geom_point(data = selected.genes.df,aes(x.pos,y.pos), size=4, fill=selected.nodes.colors,colour="black",pch=21)
+    },height = 700)
+    
+    
+    selected.genes.df$names <- gene.links[selected.genes.df$names]
+    
+    output$output_table <- renderDataTable({
+      #  selected.genes.df[,c("names","S.name","S.annotation","S.TF/Other","T.cluster")]#as.data.frame(genes.annotation.data.with.links)
+      selected.genes.df[,c("names","S.name","S.annotation","T.mapman","T.TF.Other","T.cluster")]
+    },escape=FALSE)
+  })
+  
+  ## Visualization of selected genes according to their betweeness
+  observeEvent(input$button_betweeness, {
+    print("aquí llego 8")
+    
+    betw.values <- input$betweeness_range
+    selected.genes.df <- subset(network.data, betweenness >= betw.values[1] & betweenness <= betw.values[2])
+    selected.nodes.colors <- selected.genes.df$color
+    output$networkPlot <- renderPlot({
+      ggplot(network.data, aes(x.pos,y.pos)) +
+        theme(panel.background = element_blank(),
+              panel.grid.major = element_blank(),
+              panel.grid.minor = element_blank(),
+              axis.title = element_blank(),
+              axis.text = element_blank(),
+              axis.ticks.y = element_blank()) +
+        geom_point(color=network.data$color,size=1) +
+        geom_point(data = selected.genes.df,aes(x.pos,y.pos), size=4, fill=selected.nodes.colors,colour="black",pch=21)
+    },height = 700)
+    
+    
+    selected.genes.df$names <- gene.links[selected.genes.df$names]
+    
+    output$output_table <- renderDataTable({
+      #  selected.genes.df[,c("names","S.name","S.annotation","S.TF/Other","T.cluster")]#as.data.frame(genes.annotation.data.with.links)
+      selected.genes.df[,c("names","S.name","S.annotation","T.mapman","T.TF.Other","T.cluster")]
+    },escape=FALSE)
+  })
+
+  
+  ## Visualization of selected genes according to their betweeness
+  observeEvent(input$button_eccentricity, {
+    print("aquí llego 8")
+    
+    eccen.values <- input$eccentricity_range
+    selected.genes.df <- subset(network.data, eccentricity >= eccen.values[1] & eccentricity <= eccen.values[2])
+    selected.nodes.colors <- selected.genes.df$color
+    output$networkPlot <- renderPlot({
+      ggplot(network.data, aes(x.pos,y.pos)) +
+        theme(panel.background = element_blank(),
+              panel.grid.major = element_blank(),
+              panel.grid.minor = element_blank(),
+              axis.title = element_blank(),
+              axis.text = element_blank(),
+              axis.ticks.y = element_blank()) +
+        geom_point(color=network.data$color,size=1) +
+        geom_point(data = selected.genes.df,aes(x.pos,y.pos), size=4, fill=selected.nodes.colors,colour="black",pch=21)
+    },height = 700)
+    
+    
+    selected.genes.df$names <- gene.links[selected.genes.df$names]
+    
+    output$output_table <- renderDataTable({
+      #  selected.genes.df[,c("names","S.name","S.annotation","S.TF/Other","T.cluster")]#as.data.frame(genes.annotation.data.with.links)
+      selected.genes.df[,c("names","S.name","S.annotation","T.mapman","T.TF.Other","T.cluster")]
+    },escape=FALSE)
+  })
   
 }
 
