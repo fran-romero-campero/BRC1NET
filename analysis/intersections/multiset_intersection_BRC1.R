deebc9ada645ac4fef66d6589dcf8077cf68d8ed
pedrodelosreyes <pedro.reyes@ibvf.csic.es>
2019-03-04 17:19:06 +0100

    Intersections between BRC1 target genes and all TFs target genes

--- analysis/intersections/multiset_intersection_BRC1.R
+++ analysis/intersections/multiset_intersection_BRC1.R
@@ -110,7 +110,7 @@ intersectSets <- function(tf1,tf2,set.of.genes, alias,gene.descriptions){
   intersection.data <- list()
   sets <- list(tf1, tf2, set.of.genes)
   #names(sets) <- c("cca1", "lhy", "peakZT0")
-  results <- supertest(x = sets, n = 5778)
+  results <- supertest(x = sets, n = 931)
   results.table <- summary(results)
   p.value <- tail(results.table$P.value, n=1) #Get the last p-value
   enrichment <- (results.table$Table)[["FE"]][nrow(results.table$Table)]
@@ -188,7 +188,7 @@ eccentricity.top <- gene.names[brc1.network.data$eccentricity > eccentricity.thr
 intersect2sets <- function(set1, set2, alias, gene.descriptions){
   intersection.data <- list()
   sets <- list(set1, set2)
-  results <- supertest(x = sets, n = 5778)
+  results <- supertest(x = sets, n = 931)
   results.table <- summary(results)
   p.value <- tail(results.table$P.value, n=1) #Get the last p-value
   enrichment <- (results.table$Table)[["FE"]][nrow(results.table$Table)]
@@ -427,19 +427,19 @@ feedback.outputs <- feedback.data$output
 
 
 #Initialize matrix to store the results
-intersection.table <- matrix(ncol=6, nrow = length(feedback.outputs))
+intersection.table <- matrix(ncol=6, nrow = length(ffl.outputs))
 colnames(intersection.table) <- c("master_regulator 1","master_regulator_2", "p-value", "fdr", "enrichment", "Intersection Genes") 
 head(intersection.table)
 i <- 1
 j <- 2    
 for (i in 1:length(brc1.clusters))
 {
-  for (j in 1:length(feedback.outputs))
+  for (j in 1:length(ffl.outputs))
   {
     
     
     current.cluster <- brc1.clusters[[i]]
-    current.output <- strsplit(x = as.character(feedback.outputs[[j]]), split = ",")[[1]]
+    current.output <- strsplit(x = as.character(ffl.outputs[[j]]), split = ",")[[1]]
     
     
     print(paste0("TEST",j))
@@ -450,8 +450,8 @@ for (i in 1:length(brc1.clusters))
     
     
     
-    intersection.table[j,1] <- as.character(feedback.data[j,1][1])
-    intersection.table[j,2] <- as.character(feedback.data[j,2][1])
+    intersection.table[j,1] <- as.character(ffl.data[j,1][1])
+    intersection.table[j,2] <- as.character(ffl.data[j,2][1])
     intersection.table[j,3] <- p.value
     intersection.table[j,5] <- enrichment
     intersection.table[j,6] <- paste(intersect.genes, collapse= ",")
@@ -461,319 +461,50 @@ for (i in 1:length(brc1.clusters))
   fdr.values <- p.adjust(intersection.table[,3], method = "BH")
   intersection.table[,4] <- fdr.values
   write.table(intersection.table, 
-              file=paste0("feedback_outputs_vs_clusters/feedback_outputs_vs_cluster_", i,".txt"), 
+              file=paste0("ffl_outputs_vs_clusters/ffl_outputs_vs_cluster_", i,".txt"), 
               sep="\t", row.names = FALSE, quote = FALSE)
 }
 
 
-# degree.intersections <- read.table(file="topvalues_clusters/intersections_Degree0.9.txt", header = TRUE, sep = "\t")
-# head(degree.intersections)
-# degree.intersections$fdr
-# degree.intersections$p.value
-# 
-# degree.intersections <- read.table(file="topvalues_clusters/intersections_Degree0.7.txt", header = TRUE, sep = "\t")
-# head(degree.intersections)
-# degree.intersections$fdr
-# degree.intersections$p.value
+######Intersection between BRC1 target genes and all the TFs target genes#########
+tf.names <- colnames(brc1.network.data)[21:57]
+brc1.targets <- gene.names[brc1.network.data$BRC1 == 1]
 
 
-
-
-#####Intersections between binding regions in DNA (BED files)####
-#Reading the bed files of the transcription factors
-peaks1 <- read.table(file = "../../../web_apps/peak_visualizer/data/bed_files/ELF3_ZT0_1_peaks.narrowPeak")
-head(peaks1)
-
-peaks2 <- read.table(file = "../../../web_apps/peak_visualizer/data/bed_files/PIF3_peaks.narrowPeak")
-head(peaks2)
-
-peaks3 <- read.table(file = "../../../web_apps/peak_visualizer/data/bed_files/PRR9_1_peaks.narrowPeak")
-head(peaks3)
-
-peaks.list <- list(peaks1, peaks2, peaks3)
-
-length.sets <- sapply(X = peaks.list, FUN = nrow)
-
-
-peaks.set1 <- peaks1
-peaks.set2 <- peaks2
-peaks.set2 <- random.peaks2
-
-#intersectBed functions
-intersectBed.1 <- function(peaks.set1, peaks.set2)
+#Initialize matrix to store the results
+intersection.table <- matrix(ncol=5, nrow = length(tf.names))
+colnames(intersection.table) <- c("Transcription factor", "p-value", "fdr", "enrichment", "Intersection Genes") 
+head(intersection.table)
+   
+for (i in 1:length(tf.names))
 {
-  intersection <- matrix(ncol = 3, nrow=0 )
-  current.intersection <- matrix(ncol = 3 )
-  for (i in 1:nrow(peaks.set1))
-  {
-    #Set the current peak values of set1
-    current.chr <- as.numeric(peaks.set1[i,1])
-    current.start <- peaks.set1[i,2]
-    current.end <- peaks.set1[i,3]
-    #Checking if there is intersection between the current peak and any peak of set2
-    option1 <- nrow(subset(peaks.set2, peaks.set2[,1]==current.chr & peaks.set2[,2]<=current.start & peaks.set2[,3]>=current.start))
-    option2 <- nrow(subset(peaks.set2, peaks.set2[,1]==current.chr & peaks.set2[,2]<=current.end & peaks.set2[,3]>=current.end))
+    current.target.genes <- gene.names[brc1.network.data[[i+20]] == 1]
     
-    # print(i)
+    result <- intersect2sets(set1 = brc1.targets, set2 = current.target.genes, alias = alias, gene.descriptions = description)
+    p.value <- result[1][[1]]
+    enrichment <- result[2][[1]]
+    intersect.genes <- result[3][[1]]$intersection.genes
     
-    if(option1+option2 > 0)
-    {
-      # print("HIT")
-      if(option1>0)
-      {
-        hit.peak2 <- subset(peaks.set2, peaks.set2[,1]==current.chr & peaks.set2[,2]<=current.start & peaks.set2[,3]>=current.start)
-        current.intersection[1,1] <- current.chr
-        current.intersection[1,2] <- current.start
-        current.intersection[1,3] <- hit.peak2[1,3]
-        
-      }else
-      {
-        hit.peak2 <- subset(peaks.set2, peaks.set2[,1]==current.chr & peaks.set2[,2]<=current.end & peaks.set2[,3]>=current.end)
-        current.intersection[1,1] <- current.chr
-        current.intersection[1,2] <- hit.peak2[1,2]
-        current.intersection[1,3] <- current.end
-      }
-      
-      intersection <- rbind(intersection, current.intersection)
-    }
-  }
-  return(intersection)
+    
+    
+    intersection.table[i,1] <- colnames(brc1.network.data)[i+20]
+    intersection.table[i,2] <- p.value
+    intersection.table[i,4] <- enrichment
+    intersection.table[i,5] <- paste(intersect.genes, collapse= ",")
+    
+    
 }
-
-intersectBed <- function(peaks.set1, peaks.set2)
-{
-  intersection <- matrix(ncol = 3, nrow=min(nrow(peaks.set1),nrow(peaks.set2)) )
-  j <- 0
-  for (i in 1:nrow(peaks.set1))
-  {
-    #Set the current peak values of set1
-    current.chr <- as.numeric(peaks.set1[i,1])
-    current.start <- peaks.set1[i,2]
-    current.end <- peaks.set1[i,3]
-    #Checking if there is intersection between the current peak and any peak of set2
-    hit.1 <- subset(peaks.set2, peaks.set2[,1]==current.chr & peaks.set2[,2]<=current.start & peaks.set2[,3]>=current.start)
-    option1 <- nrow(hit.1)
-    
-    if(option1>0)
-    {
-      j <- j + 1
-      intersection[j,1] <- current.chr
-      intersection[j,2] <- current.start
-      intersection[j,3] <- hit.1[1,3]
-    } else 
-    {
-      hit.2 <- subset(peaks.set2, peaks.set2[,1]==current.chr & peaks.set2[,2]<=current.end & peaks.set2[,3]>=current.end)    
-      option2 <- nrow(hit.2)
-      
-      if(option2 > 0)
-      {
-        j <- j + 1
-        intersection[j,1] <- current.chr
-        intersection[j,2] <- hit.2[1,2]
-        intersection[j,3] <- current.end
-      }
-    }
-  }
-  return(list(intersection=intersection[1:j,],size=j))
-}
-
-#Function to randomize peaks
-randomize.peaks <- function(input.peaks,chr.lengths)
-{
-  random.numbers <- runif(n = nrow(input.peaks))
-  random.starts <- ceiling(random.numbers * chr.lengths[input.peaks$V1])
-  random.ends <- random.starts + (input.peaks$V3 - input.peaks$V2)
-  
-  random.peaks <- matrix(c(input.peaks$V1,random.starts,random.ends),ncol=3) 
-  return(random.peaks)
-}
+  fdr.values <- p.adjust(intersection.table[,2], method = "BH")
+  intersection.table[,3] <- fdr.values
+  write.table(intersection.table, 
+              file="BRC1_targets_vs_all_TFs_targets.txt", 
+              sep="\t", row.names = FALSE, quote = FALSE)
 
 
 
-# The intersectBed function allow to get the intersections between two bed files. If you want to perform the
-# the intersection between three bed files, you can do it in a consecutive manner. 
-
-first <- intersectBed(peaks1, peaks2)[[1]]
-nrow(first)
-second <- intersectBed(first, peaks3)
-nrow(second)
-
-
-## Permutation of peaks.set2 (random.peaks2) and comparing with peaks.set1 ####
-chromosomes.length <- read.table(file="../../../web_apps/peak_visualizer/data/bed_files/atha_chr_lengths.txt",as.is=T)[[1]]
-number.randomisation <- 20 #100000
-
-random.intersections <- vector(mode = "numeric",length=number.randomisation) #Creating vector
-for(j in 1:number.randomisation)
-{
-  # print(j)
-  # random.peaks2 <- matrix(nrow=nrow(peaks2),ncol=3) #Matriz con 3 columnas, una para el cromosoma, otra para el comienzo y otra para el final de la marca aleatoria.
-  # for(i in 1:nrow(peaks2))
-  # {
-  #   current.chr <- peaks2[i,1][[1]] #Chr de la iésima marca real
-  #   current.start <- peaks2[i,2] #Start de la iésima marca real
-  #   current.end <- peaks2[i,3] #End de la iésima marca real
-  #   current.length <- current.end - current.start #Longitud de la iésima marca real
-  #   
-  #   chr.length <- chromosomes.length[current.chr] #Length del actual cromosoma
-  #   #Ahora genero los mismos datos para marcas aleatorias
-  #   random.start <- floor(runif(n = 1,min = 1,max = chr.length))
-  #   random.end <- random.start + current.length
-  #   
-  #   random.peaks2[i,1] <- current.chr
-  #   random.peaks2[i,2] <- random.start
-  #   random.peaks2[i,3] <- random.end
-  # }
-  
-  random.peaks2 <- randomize.peaks(input.peaks = peaks2, chr.lengths = chromosomes.length)
-  random.intersections[j] <- intersectBed(peaks.set1 = peaks1, peaks.set2 = random.peaks2)[[2]]
-  
-  p.value <- sum(random.intersections > nrow(first)) / number.randomisation
-  p.value 
-
-}
-
-
-##Loop to check the intersection of binding regions (bed files) between all the transcription factors together and store the results in a table####
-chromosomes.length <- read.table(file="../../../web_apps/peak_visualizer/data/bed_files/atha_chr_lengths.txt",as.is=T)[[1]]
-number.randomisation <- 1000
-bed.files <- list.files(path = "../../../web_apps/peak_visualizer/data/bed_files/", pattern = "peaks.narrowPeak")
-
-combinations <- expand.grid(bed.files, bed.files)
-bed.intersections <- matrix(ncol = 6, nrow = nrow(combinations))
-colnames(bed.intersections) <- c("TF1", "TF2", "p-value", "fdr", "number of intersections", "Genes" )
-
-
-txdb <- TxDb.Athaliana.BioMart.plantsmart28
-i <- 76
-total.tests <- nrow(combinations)
-# total.tests <- 50
-
-# Start the clock!
-ptm <- proc.time()
-
-for (i in 1:total.tests)
-# for (i in 1:nrow(combinations))
-{
-  # print(paste0("test number ", i, " of ", nrow(combinations)))
-  print(paste0((i/total.tests)*100, " %"))
-  peaks1 <- read.table(file = paste0("../../../web_apps/peak_visualizer/data/bed_files/", combinations[i,1]))
-  peaks2 <- read.table(file = paste0("../../../web_apps/peak_visualizer/data/bed_files/", combinations[i,2]))
-  real.intersection <- intersectBed(peaks.set1 = peaks1, peaks.set2 = peaks2)
-  if (real.intersection[[2]] > 0)
-  {
-    random.intersections <- vector(mode = "numeric",length=number.randomisation) #Creating vector
-    for(j in 1:number.randomisation)
-    {
-      print(j)
-      # random.peaks2 <- matrix(nrow=nrow(peaks2),ncol=3) #Matriz con 3 columnas, una para el cromosoma, otra para el comienzo y otra para el final de la región aleatoria.
-      # for(k in 1:nrow(peaks2))
-      # {
-      #   current.chr <- peaks2[k,1][[1]] #Chr de la iésima marca real
-      #   current.start <- peaks2[k,2] #Start de la iésima marca real
-      #   current.end <- peaks2[k,3] #End de la iésima marca real
-      #   current.length <- current.end - current.start #Longitud de la iésima marca real
-      #   
-      #   chr.length <- chromosomes.length[current.chr] #Length del actual cromosoma
-      #   #Ahora genero los mismos datos para regiones aleatorias
-      #   random.start <- floor(runif(n = 1,min = 1,max = chr.length))
-      #   random.end <- random.start + current.length
-      #   
-      #   random.peaks2[k,1] <- current.chr
-      #   random.peaks2[k,2] <- random.start
-      #   random.peaks2[k,3] <- random.end
-      # }
-      
-      random.peaks2 <- randomize.peaks(input.peaks = peaks2, chr.lengths = chromosomes.length)
-      random.intersections[j] <- intersectBed(peaks.set1 = peaks1, peaks.set2 = random.peaks2 )[[2]] 
-      
-      
-    }
-    
-    p.value <- sum(random.intersections > real.intersection[[2]]) / number.randomisation
-    if( p.value == 0)
-    {
-      p.value <- 1/number.randomisation
-    }
-    
-    if (real.intersection[[2]]==1)
-    {
-      real.intersection[[1]] <- matrix(real.intersection[[1]], ncol = 3)
-    }
-    
-    colnames(real.intersection[[1]]) <- c("chromosome", "start", "end")
-    granges.intersection <- makeGRangesFromDataFrame(real.intersection[[1]],
-                                                     keep.extra.columns=FALSE,
-                                                     ignore.strand=FALSE,
-                                                     seqinfo=NULL,
-                                                     seqnames.field="chromosome",
-                                                     start.field="start",
-                                                     end.field="end",
-                                                     starts.in.df.are.0based=FALSE)
-    
-    
-    
-    peakAnno <- annotatePeak(granges.intersection, tssRegion=c(-2000, 0),
-                             TxDb=txdb, annoDb="org.At.tair.db")
-    
-    annot.peaks <- as.data.frame(peakAnno)
-    # target.genes <- subset(annot.peaks, distanceToTSS >= 2000 | distanceToTSS <= -2000)$geneId
-    target.genes <- subset(annot.peaks, annotation != "Downstream (<1kb)" & annotation != "Downstream (1-2kb)"
-                           & annotation != "Distal Intergenic" & annotation != "Downstream (2-3kb)")$geneId
-    
-    target.genes <- paste(target.genes, collapse = ",")
-    
-    bed.intersections[i,1] <- strsplit(x = as.character(combinations[i,1]), split = "_peaks")[[1]][1]
-    bed.intersections[i,2] <- strsplit(x = as.character(combinations[i,2]), split = "_peaks")[[1]][1]
-    bed.intersections[i,3] <- p.value
-    bed.intersections[i,5] <- nrow(real.intersection)
-    bed.intersections[i,6] <- target.genes
-    
-  } else 
-  {
-    bed.intersections[i,1] <- strsplit(x = as.character(combinations[i,1]), split = "_peaks")[[1]][1]
-    bed.intersections[i,2] <- strsplit(x = as.character(combinations[i,2]), split = "_peaks")[[1]][1]
-    bed.intersections[i,3] <- NA
-    bed.intersections[i,5] <- "No intersection"
-    bed.intersections[i,6] <- NA
-  }
-  
-  
-
-}
-
-write.table(bed.intersections, file = "bed_intersections.txt", sep = "\t", row.names = FALSE)
-# Stop the clock
-proc.time() - ptm
-
-library(TxDb.Athaliana.BioMart.plantsmart28)
-library(org.At.tair.db)
-library(ChIPseeker)
-
-txdb <- TxDb.Athaliana.BioMart.plantsmart28
-colnames(real.intersection) <- c("chromosome", "start", "end")
-granges.intersection <- makeGRangesFromDataFrame(real.intersection,
-                         keep.extra.columns=FALSE,
-                         ignore.strand=FALSE,
-                         seqinfo=NULL,
-                         seqnames.field="chromosome",
-                         start.field="start",
-                         end.field="end",
-                         starts.in.df.are.0based=FALSE)
 
 
 
-peakAnno <- annotatePeak(granges.intersection, tssRegion=c(-2000, 0),
-                         TxDb=txdb, annoDb="org.At.tair.db")
 
-annot.peaks <- as.data.frame(peakAnno)
-# head(annot.peaks)
-# unique(annot.peaks$annotation)
-# target.genes <- subset(annot.peaks, distanceToTSS >= 2000 | distanceToTSS <= -2000)$geneId
-target.genes <- subset(annot.peaks, annotation != "Downstream (<1kb)" & annotation != "Downstream (1-2kb)"
-                       & annotation != "Distal Intergenic" & annotation != "Downstream (2-3kb)")$geneId
-target.genes <- paste(target.genes, collapse = ",")
-
-# cpsets(x = nrow(second), L = length.sets, n = sum(length.sets), 5778, lower.tail = FALSE) ##DUda, cuánto es n (population size)
 
 
